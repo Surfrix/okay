78301ff05802b4e546a51570b0eba674028c12f2
Surfrix <46041882+Surfrix@users.noreply.github.com>
2018-12-21 16:56:35 +0800

    Update 1

---
+++ game.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+let c = [3, 123123123, 232, -10000];
+// console.log(c["asdasd"]);
+
+if (a > b) {
+	console.log(a);
+} if (b > c[1]) {
+	console.log(c);
+} else {
+	console.log(b);
+}
